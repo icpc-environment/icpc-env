0e0eebf0cec548c59ace464ccfb684ce19ef3d2e
Keith Johnson <kjohns07@cs.fit.edu>
2015-10-29 09:20:55 -0400

    replace first boot configuration
    Way better than it previously was
    No password required for the first boot configuration
    Can load printers from a URL
    self_test script to make sure things are mostly correct

--- files/scripts/changeTeamName.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IMGDIR="/icpc"
-NAME=$(zenity --entry --title="Setup Team Name" --text="Enter Team Name(e.g. 'team201')")
-echo $NAME > $IMGDIR/TEAM
-convert $IMGDIR/wallpaper.png -gravity center -pointsize 100 -stroke '#000C' -strokewidth 2 -annotate 0 $NAME -stroke none -fill white -annotate 0 $NAME $IMGDIR/teamWallpaper.png
--- files/scripts/firstLogin.sh
+++ files/scripts/firstLogin.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
 UTILDIR="/icpc"
 
 # Set the wallpaper to the "template"
 xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $UTILDIR/wallpaper.png
-
-if [ -f $HOME/.firstLogin ]
-then
-	gksu --su-mode --description "Configure Contest System" --user icpcadmin "sudo $UTILDIR/scripts/addPrinter.sh; sudo $UTILDIR/scripts/changeTeamName.sh; sudo $UTILDIR/scripts/setupSSO.sh; sudo $UTILDIR/scripts/checkFirewall.sh;"
-	rm $HOME/.firstLogin
-fi
-
-
 
 # Reload the wallpaper(since it may have changed)
 if [ -f "$UTILDIR/teamWallpaper.png" ]; then
