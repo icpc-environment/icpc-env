b048b57972ac8961da0c4173ad93827b74191099
Keith Johnson <kjohns07@cs.fit.edu>
2015-10-29 15:03:44 -0400

    Add delay to firstLogin script
    seems to fix the gray screen on login sometimes

--- files/scripts/firstLogin.sh
+++ files/scripts/firstLogin.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 UTILDIR="/icpc"
 
-# Set the wallpaper to the "template"
-xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $UTILDIR/wallpaper.png
-
 # Reload the wallpaper(since it may have changed)
 if [ -f "$UTILDIR/teamWallpaper.png" ]; then
     xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
     xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $UTILDIR/teamWallpaper.png
-fi
+else
+  # Set the wallpaper to the "template"
+  xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
+  xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $UTILDIR/wallpaper.png
+fi  
 
 # Reload xfdesktop to get the background image showing
+sleep 5
 killall xfdesktop
 xfdesktop &
 sleep 5
