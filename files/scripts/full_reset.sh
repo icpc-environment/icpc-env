9fb463c01720ee0cb80c52d247b22b609e48eba0
Keith Johnson <kj@ubergeek42.com>
2016-10-02 21:47:27 -0400

    Add full reset script

---
+++ files/scripts/full_reset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Remove the team wallpaper
+rm -f /icpc/teamWallpaper.png
+
+# Delete the printers/printer class
+for PRINTER in $(lpstat -v | cut -d ' ' -f 3 | tr -d ':')
+do
+  lpadmin -x $PRINTER
+done
+lpadmin -x ContestPrinter
+
+# clear the user
+/icpc/scripts/deleteUser.sh
+
+# make sure the firewall is on
+ufw --force enable
+
+rm -f /icpc/setup_complete
+rm -f /icpc/TEAM
+
+# reset squid autologin
+echo "# Placeholder" > /etc/squid/autologin.conf
+chmod 640 /etc/squid/autologin.conf
+chown root:root /etc/squid/autologin.conf
+
+# clear self test report
+rm -f /icpc/self_test_report
