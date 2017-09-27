af113687ca4efe55f9dd91362234fbebaa3185d5
Keith Johnson <kj@ubergeek42.com>
2017-09-26 20:21:52 -0400

    Update eclipse to oxygen from mars

--- files/eclipse/cdt.sh
+++ files/eclipse/cdt.sh
@@ -2,7 +2,7 @@
 
 /opt/eclipse/eclipse -nosplash \
     -application org.eclipse.equinox.p2.director \
-    -repository http://download.eclipse.org/tools/cdt/releases/9.0/,http://download.eclipse.org/releases/neon/ \
+    -repository http://download.eclipse.org/tools/cdt/releases/9.3/,http://download.eclipse.org/releases/oxygen/ \
     -destination /opt/eclipse \
     -installIU org.eclipse.cdt.feature.group \
     -installIU org.eclipse.cdt.platform.feature.group | grep -v DEBUG
