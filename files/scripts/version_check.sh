46e93447ba9579fd7abdcbbc77eee05f25f74c79
Keith Johnson <kj@ubergeek42.com>
2017-09-26 22:17:54 -0400

    Add more languages to some of the self-test/version check scripts

--- files/scripts/version_check.sh
+++ files/scripts/version_check.sh
@@ -43,4 +43,16 @@ mono --version  2>&1 | tee -a $VFILE
 echo -e "\n\n==== Scala Version ====" | tee -a $VFILE
 scala -version 2>&1 | tee -a $VFILE
 
+echo -e "\n\n==== Kotlin Version ====" | tee -a $VFILE
+kotlin -version 2>&1 | tee -a $VFILE
+
+echo -e "\n\n==== Rust Version ====" | tee -a $VFILE
+rustc --version 2>&1 | tee -a $VFILE
+
+echo -e "\n\n==== NodeJS Version ====" | tee -a $VFILE
+node --version 2>&1 | tee -a $VFILE
+
+echo -e "\n\n==== OCaml Version ====" | tee -a $VFILE
+ocaml --version 2>&1 | tee -a $VFILE
+
 echo -e "Software versions written to: $VFILE\n\n"
