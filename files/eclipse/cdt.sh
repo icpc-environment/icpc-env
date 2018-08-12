48bc99da5ea8c3f8a467fbfd4f64e46fde99a2f1
Keith Johnson <kj@ubergeek42.com>
2018-08-12 15:52:14 -0400

    Update CDT for eclipse

--- files/eclipse/cdt.sh
+++ files/eclipse/cdt.sh
@@ -2,7 +2,7 @@
 
 /opt/eclipse/eclipse -nosplash \
     -application org.eclipse.equinox.p2.director \
-    -repository http://download.eclipse.org/tools/cdt/releases/9.3/,http://download.eclipse.org/releases/oxygen/ \
+    -repository http://download.eclipse.org/tools/cdt/releases/9.5/,http://download.eclipse.org/releases/photon/ \
     -destination /opt/eclipse \
     -installIU org.eclipse.cdt.feature.group \
     -installIU org.eclipse.cdt.platform.feature.group | grep -v DEBUG
