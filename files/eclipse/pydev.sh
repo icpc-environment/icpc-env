6abaaf4d094c4512567decb721de703f1027bbaf
Keith Johnson <kj@ubergeek42.com>
2018-08-12 15:52:34 -0400

    Pydev for eclipse is signed by a real cert now, so we don't need to import one

--- files/eclipse/pydev.sh
+++ files/eclipse/pydev.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-curl -o /opt/pydev_certificate.cer http://pydev.org/pydev_certificate.cer
-keytool -import -file /opt/pydev_certificate.cer -noprompt -storepass changeit -keystore /usr/lib/jvm/jdk*/jre/lib/security/cacerts
-
 /opt/eclipse/eclipse -nosplash \
     -application org.eclipse.equinox.p2.director \
     -repository http://pydev.org/updates/ \
