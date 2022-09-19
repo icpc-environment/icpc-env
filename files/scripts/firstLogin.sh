b43d4ae46ef31e407924a3a8a9a8fce56ff89a9b
Keith Johnson <kj@ubergeek42.com>
2022-09-19 19:39:53 -0400

    Add logging + make the firstLogin script more robust
    This is just responsible for setting the desktop background properly,
    but we want it to be reliable/give us logging information if it breaks

--- files/scripts/firstLogin.sh
+++ files/scripts/firstLogin.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
+# Close standard output file descriptor
+exec 1<&-
+# Close standard error file descriptor
+exec 2<&-
+
+# Open standard output as $LOG_FILE file for read and write.
+exec 1<>/tmp/firstlogin.log
+# Redirect standard error to standard output
+exec 2>&1
+
+
 UTILDIR="/icpc"
 
-# Reload the wallpaper(since it may have changed)
 if [ -f "$UTILDIR/teamWallpaper.png" ]; then
-    xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
-    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $UTILDIR/teamWallpaper.png
-    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -s 3
+  BACKGROUND="$UTILDIR/teamWallpaper.png"
 else
   # Set the wallpaper to the "template"
-  xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
-  xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $UTILDIR/wallpaper.png
-  xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -s 3
+  BACKGROUND="$UTILDIR/wallpaper.png"
 fi
 
-# Reload xfdesktop to get the background image showing
+# wait for xfdesktop to be loaded
+echo "Waiting for xfdesktop to be running"
+while ! pgrep xfdesktop; do
+  sleep 1
+done
+
+# wait a few moments for things to load initally
 sleep 5
+
+echo "Update the desktop background properties"
+xfconf-query -c xfce4-desktop -p /backdrop/screen0 -rR
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/last-image --create -t string -s $BACKGROUND
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path --create -t string -s $BACKGROUND
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style --create -t int -s 3
+
+# Reload xfdesktop to get the background image showing (--reload doesn't work, have to --quit first...)
+echo "Reload xfdesktop to refresh the background"
+sleep 5
+xfdesktop --quit
 timeout 5 xfdesktop --reload
 sleep 5
+xfdesktop --quit
 timeout 5 xfdesktop --reload
+
