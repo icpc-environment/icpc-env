1be4d49c5e870f9e90d5cfa68fff510997046369
Keith Johnson <kj@ubergeek42.com>
2022-09-22 17:46:24 -0400

    Update create_baseimg + add imageadmin ssh keys
    The keys are fine to add, because the imageadmin user gets wiped when
    the image is finalized.

--- create_baseimg.sh
+++ create_baseimg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Settings
-ISO64="ubuntu-18.04.3-server-amd64.iso"
-OUT64="unattended-ubuntu-18.04.3-amd64.iso"
+ISO64="ubuntu-20.04.5-live-server-amd64.iso"
+OUT64="unattended-${ISO64}"
 IMG64="base-amd64.img"
 
 TMPDIR="tmp"
-KICKSTART="configs/1804_ks.cfg"
-PRESEED="configs/1804_install.seed"
+USERDATA="configs/2004_autoinstall.yaml"
+METADATA="configs/2004_metadata"
 
 function usage() {
   echo "Usage: create_baseimage.sh [-s size]"
@@ -35,72 +35,80 @@ while [[ $# -ge 1 ]]; do
 done
 
 # Default image size 14700M(fits on an 16G flash drive)
-IMGSIZE=${IMGSIZE:-14700M}
-
-CONTENTSDIR="$TMPDIR/contents"
-rm -rf "$CONTENTSDIR"
-mkdir -p "$CONTENTSDIR"
+# IMGSIZE=${IMGSIZE:-14700M}
+# Default image size 28500M(fits on an 32G flash drive)
+IMGSIZE=${IMGSIZE:-28500M}
 
 
-#Use bsdtar if possible to extract(no root required)
-if hash bsdtar 2>/dev/null; then
-  bsdtar xfp $ISO -C $CONTENTSDIR
-  chmod -R u+w "$CONTENTSDIR"
-else
-  # mount the iso, then copy the contents
-  LOOPDIR="$TMPDIR/iso"
-  mkdir -p "$LOOPDIR"
-  sudo mount -o loop "$ISO" "$LOOPDIR"
-  cp -rT "$LOOPDIR" "$CONTENTSDIR"
-  sudo umount "$LOOPDIR"
-fi
+function create_unattended_iso() {
+  CONTENTSDIR="$TMPDIR/contents"
+  rm -rf "$CONTENTSDIR"
+  mkdir -p "$CONTENTSDIR"
 
 
-# Skip language selection menu
-chmod u+w $CONTENTSDIR/isolinux
-echo "en" > "$CONTENTSDIR/isolinux/lang"
+  #Use bsdtar if possible to extract(no root required)
+  if hash bsdtar 2>/dev/null; then
+    bsdtar xfp $ISO -C $CONTENTSDIR
+    chmod -R u+w "$CONTENTSDIR"
+  else
+    # mount the iso, then copy the contents
+    LOOPDIR="$TMPDIR/iso"
+    mkdir -p "$LOOPDIR"
+    sudo mount -o loop "$ISO" "$LOOPDIR"
+    cp -rT "$LOOPDIR" "$CONTENTSDIR"
+    sudo umount "$LOOPDIR"
+  fi
 
-cp "$KICKSTART" "$CONTENTSDIR/ks.cfg"
-cp "$PRESEED" "$CONTENTSDIR/ks.preseed"
 
-cat <<EOF > "$CONTENTSDIR/isolinux/txt.cfg"
+  # Skip language selection menu
+  chmod u+w $CONTENTSDIR/isolinux
+  echo "en" > "$CONTENTSDIR/isolinux/lang"
+
+  mkdir -p "$CONTENTSDIR/autoinst"
+  cp "$USERDATA" "$CONTENTSDIR/autoinst/user-data"
+  cp "$METADATA" "$CONTENTSDIR/autoinst/meta-data"
+
+  cat <<EOF > "$CONTENTSDIR/isolinux/txt.cfg"
 default install
 label install
   menu label ^Install Ubuntu Server (Unattended)
-  kernel /install/vmlinuz
-  append initrd=/install/initrd.gz ks=cdrom:/ks.cfg preseed/file=/cdrom/ks.preseed --
+  kernel /casper/vmlinuz
+  append initrd=/casper/initrd autoinstall ds=nocloud-net;seedfrom=/cdrom/autoinst/ net.ifnames=0 --
 label memtest
   menu label Test ^memory
   kernel /install/mt86plus
 EOF
 
-cat <<EOF > "$CONTENTSDIR/isolinux/isolinux.cfg"
+  cat <<EOF > "$CONTENTSDIR/isolinux/isolinux.cfg"
 # D-I config version 2.0
 path
 include menu.cfg
 default vesamenu.c32
 prompt 0
-# 5 seconds
-timeout 50
+# 2.5 seconds
+timeout 25
 ui gfxboot bootlogo
 EOF
-echo "en" > "$CONTENTSDIR/isolinux/lang"
-set -x
-mkisofs -r -V "ATTENDLESS_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -quiet -o $OUTISO $CONTENTSDIR
-set +x
+  echo "en" > "$CONTENTSDIR/isolinux/lang"
+  set -x
+  mkisofs -r -V "ATTENDLESS_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -quiet -o $OUTISO $CONTENTSDIR
+  set +x
 
-# cleanup
-rm -rf "$CONTENTSDIR"
+  # cleanup
+  rm -rf "$CONTENTSDIR"
+}
+
+create_unattended_iso
 
 # Install that base image
 rm -f "output/$IMG"
 set -x
-qemu-img create -f raw -o size="$IMGSIZE" "output/$IMG"
+qemu-img create -f qcow2 -o size="$IMGSIZE" "output/$IMG"
 qemu-system-x86_64 \
   --enable-kvm -m 1024 -global isa-fdc.driveA= \
-  -drive file="output/$IMG",index=0,media=disk,format=raw \
+  -drive file="output/$IMG",index=0,media=disk,format=qcow2 \
   -cdrom $OUTISO -boot order=d \
   -net nic -net user,hostfwd=tcp::5222-:22,hostfwd=tcp::5280-:80 \
-  -vga qxl -vnc :0 -spice port=5901,disable-ticketing \
+  -vga qxl -vnc :0 \
   -usbdevice tablet
 # -global isa-fdc.driveA= is used to disable floppy drive(gets rid of a warning message)
